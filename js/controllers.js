07456f6b270f1e5c27c5bfa6634137920c13113f
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-08-13 22:28:32 +0530

    First

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('Home', ['$scope', function($scope) {
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
