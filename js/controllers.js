ba7e36a8fc02e1dc29e50f938766ff15cb6ad8b4
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-08-13 23:30:00 +0530

    11:29 PM 8/13/2014

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('Home', ['$scope', function($scope) {
+.controller('Home', ['$scope', function($scope) {
+	$scope.news = [];
+	function render(posts){
+		$scope.$apply(function(){
+			$scope.news = posts.feed.entries;
+			//alert("rendering" + JSON.stringify($scope.news));			
+		});
+	}
+	window.Feed({
+		url: 'http://feeds.feedburner.com/mathrubhumi',
+		limit: 0,
+		callback: render
+	});
+}])
+.controller('MyCtrl2', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+}]);
