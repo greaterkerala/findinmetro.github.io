61f81b33ab6a9d6f5a8ef4970dde1955d679858d
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-09-14 22:28:48 +0530

    1
    1

--- data/kerala/gallery/video/home-videos.js
+++ data/kerala/gallery/video/home-videos.js
@@ -5,6 +5,6 @@
 	{"width":"230", "height":"189", "src":"http://www.youtube.com/embed/oc8oOksJHZk", "frameborder":"0" },  
 	{"width":"230", "height":"189", "src":"http://www.youtube.com/embed/hxU_bsel_PQ", "frameborder":"0" },  
 	{"width":"230", "height":"189", "src":"http://www.youtube.com/embed/VXZMrqdDNlQ", "frameborder":"0" },  
-	{"width":"230", "height":"189", "src":"http://www.youtube.com/embed/t0YxFMeFUbQ", "frameborder":"0" },
+	{"width":"230", "height":"189", "src":"http://www.youtube.com/embed/t0YxFMeFUbQ", "frameborder":"0" }
 	]
 }
