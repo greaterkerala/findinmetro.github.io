d5ec80b2ec1e29c3c462fdae5d970e4a37379f38
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-09-14 22:27:32 +0530

    Revert "2"
    This reverts commit 334446ceddbf39073b9bbe3de79080cc7dabe068.

--- components/version/filters.js
+++ components/version/filters.js
@@ -2,22 +2,17 @@
 
 angular.module('myApp.version.interpolate-filter', [])
 .filter('unsafe', function($sce) {
-	return function(val) {
-		return $sce.trustAsHtml(val);
-	};
+    return function(val) {
+        return $sce.trustAsHtml(val);
+    };
 })
 .filter('trusted', function ($sce) {
-	return function(url) {
-		return $sce.trustAsResourceUrl(url);
-	};
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
 })
 .filter('interpolate', ['version', function(version) {
-	return function(text) {
-		return String(text).replace(/\%VERSION\%/mg, version);
-	};
-}])
-.filter('slice', function() {
-	return function(arr, start, end) {
-		return (arr || []).slice(start, end);
-	};
-});
+  return function(text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  };
+}]);
