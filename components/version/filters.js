c32632b0f6e78d6e109f7d4b27cd5796351b7a21
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-09-16 21:36:46 +0530

    1

--- components/version/filters.js
+++ components/version/filters.js
@@ -2,17 +2,35 @@
 
 angular.module('myApp.version.interpolate-filter', [])
 .filter('unsafe', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val);
-    };
+	return function(val) {
+		return $sce.trustAsHtml(val);
+	};
 })
 .filter('trusted', function ($sce) {
-    return function(url) {
-        return $sce.trustAsResourceUrl(url);
-    };
+	return function(url) {
+		return $sce.trustAsResourceUrl(url);
+	};
 })
 .filter('interpolate', ['version', function(version) {
-  return function(text) {
-    return String(text).replace(/\%VERSION\%/mg, version);
-  };
-}]);
+	return function(text) {
+		return String(text).replace(/\%VERSION\%/mg, version);
+	};
+}])
+.filter('partition', function() {
+	var cache = {};
+	var filter = function(arr, size) {
+		if (!arr) { return; }
+		var newArr = [];
+		for (var i=0; i<arr.length; i+=size) {
+			newArr.push(arr.slice(i, i+size));
+		}
+		var arrString = JSON.stringify(arr);
+		var fromCache = cache[arrString+size];
+		if (JSON.stringify(fromCache) === JSON.stringify(newArr)) {
+			return fromCache;
+		}
+		cache[arrString+size] = newArr;
+		return newArr;
+	};
+	return filter;
+});
