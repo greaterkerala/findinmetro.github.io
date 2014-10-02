20f47204d146e3bdfa9bd5926396d8e05b9a621a
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-10-02 19:03:31 +0530

    7:03 PM 10/2/2014

--- data/kerala/travel/districts.js
+++ data/kerala/travel/districts.js
@@ -2,53 +2,185 @@
 	"list": 
 	[
 	{
-		"Title":"Title1", 
-		"Brief_Description":"Brief Description", 
-		"Thumbnail_Image":"http://lorempixel.com/600/200/people", 
-		"Thumbnail_Image_Alt":"thumbnail image alt", 
-		"Landing_Image":"http://lorempixel.com/600/200/people", 
-		"Landing_Image_Alt":"landing image alt", 
+		"Title":"Alappuzha", 
+		"Brief_Description":"The district is also known for its communist traditions. It is home to the Punnapra-Vayalar uprising against the British and also the revolt against the Feudal raj. Alappuzha is strongly connected by waterways to various other parts of Kerala, including the famous tourist destination, Kumarakom.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/alappuzha/320px-House_Boat_View_DSW.jpg", 
+		"Thumbnail_Image_Alt":"House Boat View", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
 		"Actions": 
 		[
-		{"Title": "Action 1", "Url":""}
-		]
-	}  ,
-	{
-		"Title":"Title1", 
-		"Brief_Description":"Brief Description", 
-		"Thumbnail_Image":"http://lorempixel.com/600/200/people", 
-		"Thumbnail_Image_Alt":"thumbnail image alt", 
-		"Landing_Image":"http://lorempixel.com/600/200/people", 
-		"Landing_Image_Alt":"landing image alt", 
-		"Actions": 
-		[
-		{"Title": "Action 1", "Url":""}
-		]
-	}  ,
-	{
-		"Title":"Title2", 
-		"Brief_Description":"Brief Description sdf asdfasdfa sfasdfsdfasfdasfsadfasdfalsdkfj sdfsd fsdf sdfsdf sdf sadfsad fsdfsdfsd fsdf sdfsdf sdf sadfsad fsdfsdfsd fsdf sdfsdf sdf sadfsad fsdfsdfsd fsdf sdfsdf sdf sadfsad fsdf", 
-		"Thumbnail_Image":"http://lorempixel.com/600/200/people", 
-		"Thumbnail_Image_Alt":"thumbnail image alt", 
-		"Landing_Image":"http://lorempixel.com/600/200/people", 
-		"Landing_Image_Alt":"landing image alt", 
-		"Actions": 
-		[
-		{"Title": "Action 1", "Url":""}
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=9.49,76.49&q=9.49,76.49&hl=en&t=m&z=12"}
 		]
 	} ,
 	{
-		"Title":"Title3", 
-		"Brief_Description":"Brief Description", 
-		"Thumbnail_Image":"http://lorempixel.com/600/200/people", 
-		"Thumbnail_Image_Alt":"thumbnail image alt", 
-		"Landing_Image":"http://lorempixel.com/600/200/people", 
-		"Landing_Image_Alt":"landing image alt", 
+		"Title":"Ernakulam (also Kochi Cochin)", 
+		"Brief_Description":"Ernakulam is known as the commercial capital of Kerala. The district is famous for its ancient temples, churches, and mosques. ", 
+		"Thumbnail_Image":"data/kerala/travel/districts/ernakulam/kochifishingnet.jpg", 
+		"Thumbnail_Image_Alt":"Chinese Fishing Net", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
 		"Actions": 
 		[
-		{"Title": "Action 1", "Url":""}
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=10,76.33&q=10,76.33&hl=en&t=m&z=12"}
 		]
-	}   
+	} ,
+	{
+		"Title":"Idukki", 
+		"Brief_Description":"The name of the district is derived from the Malayalam word Idukku ( ഇടുക്ക് ), meaning narrow gorge. Rugged mountains and forests cover about 97 percent of the total area of the District. ", 
+		"Thumbnail_Image":"data/kerala/travel/districts/idukki/320px-Idukki_scenic.JPG", 
+		"Thumbnail_Image_Alt":"Idukki scenic beauty", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=9.85,76.94&q=9.85,76.94&hl=en&t=m&z=12"}
+		]
+	} ,
+	{
+		"Title":"Kannur", 
+		"Brief_Description":"The district is a major centre of Theyyam, a ritual dance of northern Kerala. Small shrines known as kavus associated with the Theyyam dot the district. It was the capital of the one and only Muslim kingdom, Arakkal Sulthanate, in the state of Kerala.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/kannur/Thiruvappana_and_Muthappan.jpg", 
+		"Thumbnail_Image_Alt":"Folk dance", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=11.8689,75.35546&q=11.8689,75.35546&hl=en&t=m&z=12"}
+		]
+	} ,
+	{
+		"Title":"Kasaragod", 
+		"Brief_Description":"Kasaragod is the northernmost district of Kerala and world renowned for its coir and hand-loom industries. It is a region where three 'dream-lands', like (the rest of) Kerala, Tulunadu and Coorg meet. The district is filled with rivers, hillocks, beaches, shrines and forts.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/kasaragod/320px-Bakel_ratnu.jpg", 
+		"Thumbnail_Image_Alt":"Bekal Fort Beach", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=12.5,75&q=12.5,75&hl=en&t=m&z=12"}
+		]
+	} 	,
+	{
+		"Title":"Kollam", 
+		"Brief_Description":"The district has a cross-section of Kerala's natural attributes; it is endowed with a long coastline, a major Arabian Sea seaport and an inland lake (Ashtamudi Lake). Kollam is the capital of Kerala's cashew industry. Plains, mountains, lakes, lagoons and backwaters, forests, farmland and rivers make up the topography of the district.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/kollam/Paravur_backwaters_&_beaches.jpg", 
+		"Thumbnail_Image_Alt":"Scenic beauty of backwaters and beaches", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=8.8,76.6&q=8.8,76.6&hl=en&t=m&z=12"}
+		]
+	} 	,
+	{
+		"Title":"Kollam", 
+		"Brief_Description":"The district has a cross-section of Kerala's natural attributes; it is endowed with a long coastline, a major Arabian Sea seaport and an inland lake (Ashtamudi Lake). Kollam is the capital of Kerala's cashew industry. Plains, mountains, lakes, lagoons and backwaters, forests, farmland and rivers make up the topography of the district.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/kollam/Paravur_backwaters_&_beaches.jpg", 
+		"Thumbnail_Image_Alt":"Scenic beauty of backwaters and beaches", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=8.8,76.6&q=8.8,76.6&hl=en&t=m&z=12"}
+		]
+	} 	,
+	{
+		"Title":"Kottayam", 
+		"Brief_Description":"The first district to achieve 100% literacy rate in the whole of India.  Panoramic backwater stretches, lush paddy fields, highlands, hills and hillocks, rubber plantations and places associated with many legends given Kottayam District the enviable title: The land of letters, legends, latex and lakes.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/kottayam/320px-House_boat_1.JPG", 
+		"Thumbnail_Image_Alt":"House Boat in Kumarakam", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=9.595,76.531&q=9.595,76.531&hl=en&t=m&z=12"}
+		]
+	}  	,
+	{
+		"Title":"Kozhikode", 
+		"Brief_Description":"Calicut is the anglicized form of Kalikooth, the name used by Arabs to refer to Kozhikode. ", 
+		"Thumbnail_Image":"data/kerala/travel/districts/kozhikode/320px-Kappad_beach_2.jpg", 
+		"Thumbnail_Image_Alt":"View of Kappad beach", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=9.595,76.531&q=9.595,76.531&hl=en&t=m&z=12"}
+		]
+	},
+	{
+		"Title":"Malappuram", 
+		"Brief_Description":"The classic medieval center of learning Vedic and local politics, Thirunavaya, home of the traditional Ayurveda medicine, Kottakkal and one of the oldest centre of education of Islam the region, Ponnani are situated in Malappuram district. Malappuram district comprises a vast wildlife collection and a number of small hills, forests, little rivers and streams flowing to the west, backwaters and paddy, arecanut, cashew nut, pepper, ginger, pulses, coconut, banana, tapioca, and rubber plantations. ", 
+		"Thumbnail_Image":"data/kerala/travel/districts/malappuram/320px-Kadalundi_Bird_Sanctuary.jpg", 
+		"Thumbnail_Image_Alt":"Kadalundi Bird Sanctuary", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=11.03,76.05&q=11.03,76.05&hl=en&t=m&z=12"}
+		]
+	},
+	{
+		"Title":"Palakkad", 
+		"Brief_Description":"Palakkad is the gateway to Kerala due to the presence of the Palakkad Gap, in the Western Ghats. Palakkad is the gateway to Kerala due to the presence of the Palakkad Gap, in the Western Ghats. ", 
+		"Thumbnail_Image":"data/kerala/travel/districts/palakkad/220px-KillikkurussiMahadevaKshetram.jpg", 
+		"Thumbnail_Image_Alt":"", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=10.783333,76.65&q=10.783333,76.65&hl=en&t=m&z=12"}
+		]
+	},
+	{
+		"Title":"Pathanamthitta", 
+		"Brief_Description":"Pathanamthitta is known as the headquarters of pilgrimage tourism, for the large number of pilgrims it receives. Having three rivers and a forest cover which includes a tiger reserve, the district also attracts many wildlife and nature enthusiasts. The district tourism gave Pathanamathitta district the caption 'The Abode of God' because of its importance in pilgrimage. ", 
+		"Thumbnail_Image":"data/kerala/travel/districts/pathanamthitta/240px-Aranmula_Temple.JPG", 
+		"Thumbnail_Image_Alt":"Temple at Aranmula", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=9.27,76.78&q=9.27,76.78&hl=en&t=m&z=12"}
+		]
+	},
+	{
+		"Title":"Thiruvananthapuram", 
+		"Brief_Description":"The headquarters is the city of Thiruvananthapuram (Trivandrum) which is also the capital city of Kerala. The city gets its name from the word Thiru-anantha-puram, meaning the Abode of Lord Anantha.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/thiruvananthapuram/320px-TVM_aps_temple.jpg", 
+		"Thumbnail_Image_Alt":"Sree Padmanabhaswamy temple in Thiruvananthapuram", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=8.48,76.94&q=8.48,76.94&hl=en&t=m&z=11"}
+		]
+	},
+	{
+		"Title":"Thrissur", 
+		"Brief_Description":"Thrissur is known as the cultural capital of Kerala, and the land of Poorams. The district is famous for its ancient temples, churches, and mosques. Thrissur Pooram is the most colourful and spectacular temple festival in Kerala.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/thrissur/281px-ThrissurPooram-Kuda.jpg", 
+		"Thumbnail_Image_Alt":"Kuda Mattam during Thrissur pooram festival.", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=10.52,76.21&q=10.52,76.21&hl=en&t=m&z=12"}
+		]
+	},
+	{
+		"Title":"Wayanad", 
+		"Brief_Description":"Wayanad district stands in the southern tip of the Deccan Plateau and its chief glory is the majestic Western Ghats with lofty ridges interspersed with dense forest, tangled jungles and deep valleys.", 
+		"Thumbnail_Image":"data/kerala/travel/districts/wayanad/180px-Chembra.JPG", 
+		"Thumbnail_Image_Alt":"Chembra peak:View from Hrudaya Saras", 
+		"Landing_Image":"", 
+		"Landing_Image_Alt":"", 
+		"Actions": 
+		[
+		{"Title": "Map", "Url":"http://maps.google.com/maps?ll=11.605,76.083&q=11.605,76.083&hl=en&t=m&z=12"}
+		]
+	}
 
 	]
 }
