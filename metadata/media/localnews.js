36bc443736126d968c03c3b405359952bfbc8834
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-10-02 09:53:34 +0530

    1

---
+++ metadata/media/localnews.js
@@ -0,0 +1,21 @@
+{
+	"topscrollmodules":
+	[
+
+	],
+	"leftpanemodules" : 
+	[
+	],
+	"contentpanemodules" : 
+	[
+	{"type":"localnews",  "Title": "Local News",   "Data": "https://news.google.com/news/feeds?pz=1&cf=all&ned=in&hl=en&geo=Ernakulam&output=rss", "limit":"50" }
+	],
+	"rightpanemodules":
+	[
+	],
+
+	"bottomscrollmodules":
+	[	
+	]
+
+}
