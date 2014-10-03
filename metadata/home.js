0c260d7f7db971d82af3fc98313cfed2bd0c8cb7
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-10-03 09:25:56 +0530

    1

--- metadata/home.js
+++ metadata/home.js
@@ -5,9 +5,8 @@
 	],
 	"leftpanemodules" : 
 	[
-	{"type":"weather", "Title": "Current Weather", "Data": {"height": "400px", "lat": "9.7415985", "lon":"76.5302717"}}, 				 
-	{"type":"dynamicyoutube", "Title": "Latest Videos", "Data": {"height": "300px", "url":"https://gdata.youtube.com/feeds/api/videos?q=kerala%20landscape&orderby=viewCount&max-results=10&alt=json"}}, 
-	{"type":"staticyoutube", "Title": "Editor Picks", "Data": {"url":"data/kerala/gallery/video/home-videos.js"}}
+	{"type":"weather", "Title": "Current Weather", "Data": {"height": "400px", "lat": "9.7415985", "lon":"76.5302717"}}
+
 	],
 	"contentpanemodules" : 
 	[
