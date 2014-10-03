fdb72787b45a68dba5c62721686959b1752410bb
SNMBA <smallnmediumbusinessapp@gmail.com>
2014-10-03 10:30:38 +0530

    7

--- data/kerala/travel/districts.js
+++ data/kerala/travel/districts.js
@@ -52,7 +52,7 @@
 	{
 		"Title":"Kasaragod", 
 		"Brief_Description":"Kasaragod is the northernmost district of Kerala and world renowned for its coir and hand-loom industries. It is a region where three 'dream-lands', like (the rest of) Kerala, Tulunadu and Coorg meet. The district is filled with rivers, hillocks, beaches, shrines and forts.", 
-		"Thumbnail_Image":"data/kerala/travel/districts/kasaragod/320px-Bakel_ratnu.jpg", 
+		"Thumbnail_Image":"data/kerala/travel/districts/kasaragod/320px-Bakel_ratnu.JPG", 
 		"Thumbnail_Image_Alt":"Bekal Fort Beach", 
 		"Landing_Image":"", 
 		"Landing_Image_Alt":"", 
